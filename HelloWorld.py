a7dc6e3d7c87ab82e143583c2140fa899bb7ea2a
WalterBrito <waltter.britto@gmail.com>
2016-10-01 09:56:09 -0300

    Simple Python Hello World App

---
+++ HelloWorld.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import os
+from flask import Flask
+
+app = Flask(_name_)
+
+
+@app.route("/")
+def index():
+    return "<h1>Hello World</h1>"
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
